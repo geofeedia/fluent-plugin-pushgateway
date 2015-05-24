1371d25c153ea13f3f097d93d3d2f4cdf7e98b57
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2015-05-24 23:24:45 +0900

    tmp

--- lib/fluent/plugin/out_prometheus_pushgateway.rb
+++ lib/fluent/plugin/out_prometheus_pushgateway.rb
@@ -8,6 +8,11 @@ module Fluent
 
     config_param :host, :string,  :default => '127.0.0.1'
     config_param :port, :integer, :default => 9091
+    config_param :job_key, :string, :default => 'job'
+    config_param :instance_key, :string, :default => 'instance'
+    config_param :name_key, :string, :default => 'name'
+    config_param :label_keys, :string, :default => '' # comma separated
+    config_param :value_key, :string, :default => 'value'
 
     def initialize
       super
@@ -22,10 +27,10 @@ module Fluent
       registry = Prometheus::Client::Registry.new
 
       es.each do |time, record|
-        gauge = Prometheus::Client::Gauge.new(record['name'].to_sym, "dummy")
+        gauge = Prometheus::Client::Gauge.new(record[@name_key].to_sym, "dummy")
         registry.register(gauge)
-        gauge.set({}, record['value'])
-        Prometheus::Client::Push.new(record['job'], record['instance'], @gateway).add(registry)
+        gauge.set({}, record[@value_key])
+        Prometheus::Client::Push.new(record[@job_key], record[@instance_key], @gateway).add(registry)
       end
 
       chain.next
